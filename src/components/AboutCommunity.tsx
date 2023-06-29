512748f892091bdc8978265cd76506f6931a706d
gastraderr <gaslimits@gmail.com>
2023-06-29 00:04:17 -0400

    commit before branch fuckening

--- src/components/AboutCommunity.tsx
+++ src/components/AboutCommunity.tsx
@@ -15,24 +15,23 @@ interface Community {
   id: string;
 }
 
-const AboutCommunity = async ({
-  children,
-  params: slug ,
-}: {
+interface AboutCommunityProps {
   children: ReactNode;
   params: { slug: string };
-}) => {
-    const session = await getAuthSession();
-    
+}
+
+const AboutCommunity = async ({ children, params }: AboutCommunityProps) => {
+  const session = await getAuthSession();
+
   const community = await db.community.findFirst({
-    where: { name: slug },
+    where: { name: params.slug },
     select: {
       subscribers: true,
       createdAt: true,
       name: true,
       creatorId: true,
-      id: true
-    }
+      id: true,
+    },
   });
   // console.log("DA COMMUNITY IS: ", community)
   const subscription = !session?.user
@@ -40,7 +39,7 @@ const AboutCommunity = async ({
     : await db.subscription.findFirst({
         where: {
           community: {
-            name: slug,
+            name: params.slug,
           },
           user: {
             id: session.user.id,
@@ -49,57 +48,53 @@ const AboutCommunity = async ({
       });
 
   const isSubscribed = !!subscription;
-  
 
   if (!community) return notFound();
-      
 
-
-    return(
-          <div className="overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last">
-            <div className="px-6 py-4 bg-violet-100">
-              <p className="font-semibold py-3">About c/{community.name}</p>
-            </div>
-            <dl className="divide-y divide-gray-100 px-6 py-4 text-sm leading-6 bg-white">
-              <div className="flex justify-between gap-x-4 py-3">
-                <dt className="text-gray-500">Created</dt>
-                <dd className="text-gray-700">
-                  <time dateTime={community.createdAt.toDateString()}>
-                    {format(community.createdAt, "MMMM d, yyyy")}
-                  </time>
-                </dd>
-              </div>
-              <div className="flex justify-between gap-x-4 py-3">
-                <dt className="text-gray-500">Members</dt>
-                <dd className="flex items-start gap-x-2">
-                  <div className="text-gray-900">{community.subscribers.length}</div>
-                </dd>
-              </div>
-              {community.creatorId === session?.user?.id ? (
-                <div className="flex justify-between gap-x-4 py-3">
-                  <dt className="text-gray-500">You created this community</dt>
-                </div>
-              ) : null}
-
-              {community.creatorId !== session?.user?.id ? (
-                
-                <SubscribeLeaveToggle
-                  isSubscribed={isSubscribed}
-                  
-                  communityId={community.id}
-                  communityName={community.name}
-                />
-              ) : null}
-              <Link
-                className={buttonVariants({
-                  variant: "outline",
-                  className: "w-full mb-6",
-                })}
-                href={`c/${slug}/submit`}
-              >
-                Create Post
-              </Link>
-            </dl>
+  return (
+    <div className="overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last">
+      <div className="px-6 py-4 bg-violet-100">
+        <p className="font-semibold py-3">About c/{community.name}</p>
+      </div>
+      <dl className="divide-y divide-gray-100 px-6 py-4 text-sm leading-6 bg-white">
+        <div className="flex justify-between gap-x-4 py-3">
+          <dt className="text-gray-500">Created</dt>
+          <dd className="text-gray-700">
+            <time dateTime={community.createdAt.toDateString()}>
+              {format(community.createdAt, "MMMM d, yyyy")}
+            </time>
+          </dd>
+        </div>
+        <div className="flex justify-between gap-x-4 py-3">
+          <dt className="text-gray-500">Members</dt>
+          <dd className="flex items-start gap-x-2">
+            <div className="text-gray-900">{community.subscribers.length}</div>
+          </dd>
+        </div>
+        {community.creatorId === session?.user?.id ? (
+          <div className="flex justify-between gap-x-4 py-3">
+            <dt className="text-gray-500">You created this community</dt>
           </div>
-          )}
+        ) : null}
+
+        {community.creatorId !== session?.user?.id ? (
+          <SubscribeLeaveToggle
+            isSubscribed={isSubscribed}
+            communityId={community.id}
+            communityName={community.name}
+          />
+        ) : null}
+        <Link
+          className={buttonVariants({
+            variant: "outline",
+            className: "w-full mb-6",
+          })}
+          href={`c/${params.slug}/submit`}
+        >
+          Create Post
+        </Link>
+      </dl>
+    </div>
+  );
+};
 export default AboutCommunity;
