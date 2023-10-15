f91449f241ea9c2096c069e97258cfbada4b8db4
gastraderr <gaslimits@gmail.com>
2023-10-15 15:12:36 -0400

    ok

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -39,7 +39,7 @@ export async function UserAccountNav({ user }: UserAccountNavProps) {
         />
       </DropdownMenuTrigger>
       <DropdownMenuContent className="bg-white" align="end">
-        <div className="flex items-center justify-center gap-2 p-2 ">
+        <div className="flex items-center justify-start gap-2 p-2 ">
           <div className="flex flex-col space-y-1 leading-none">
             {user.name && <p className="font-medium">{user.name}</p>}
             {user.email && (
